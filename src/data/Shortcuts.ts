6410e8377e1152395943b3a4b396ab0ae0b35372
glenn-robert-netstratum <glennrobert@netstratum.com>
2026-05-28 23:07:33 +0530

    feat(shortcut): Added shortcuts to the project

---
+++ src/data/Shortcuts.ts
@@ -0,0 +1,30 @@
+export const shortcuts = [
+  {
+    name: "YouTube",
+    image:
+      "https://cdn-icons-png.flaticon.com/512/1384/1384060.png",
+  },
+
+  {
+    name: "Maps",
+    image:
+      "https://cdn-icons-png.flaticon.com/512/2991/2991231.png",
+  },
+
+  {
+    name: "Spotify",
+    image:
+      "https://cdn-icons-png.flaticon.com/512/174/174872.png",
+  },
+
+  {
+    name: "Reddit",
+    image:
+      "https://cdn-icons-png.flaticon.com/512/2111/2111589.png",
+  },
+
+  {
+    name: "Gmail",
+    image:"https://cdn-icons-png.flaticon.com/512/732/732200.png"
+  }
+]
