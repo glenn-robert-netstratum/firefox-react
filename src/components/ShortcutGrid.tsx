c7cd66313a7383a9ae2a52e9211ddd23e711ff8a
glenn-robert-netstratum <glennrobert@netstratum.com>
2026-06-01 15:35:07 +0530

    feat(Shortcuts): Added delete ,edit and add buttons and functionalities

--- src/components/ShortcutGrid.tsx
+++ src/components/ShortcutGrid.tsx
@@ -1,11 +1,130 @@
-import { shortcuts } from "@/data/Shortcuts"
+import { defaultShortcuts } from "@/data/Shortcuts"
+import { useEffect, useState } from "react"
+import ModalOverlay from "@/components//ModalOverlay"
 
 import {
   Card,
   CardContent,
 } from "@/components/ui/card"
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+interface Shortcut {
+  id: number
+  name: string
+  image: string
+  url: string
+}
+
 function ShortcutGrid() {
+  const [showModal, setShowModal] = useState(false)
+  const [title, setTitle] = useState("")
+  const [url, setUrl] = useState("")
+  const [image, setImage] = useState("")
+  const [editingId, setEditingId] = useState<number | null>(null)
+  const [shortcuts, setShortcuts] = useState<Shortcut[]>(() => {
+
+    const storedShortcuts =
+      localStorage.getItem("shortcuts")
+
+    if (storedShortcuts) {
+      return JSON.parse(storedShortcuts) as Shortcut[]
+    }
+
+    localStorage.setItem(
+      "shortcuts",
+      JSON.stringify(defaultShortcuts)
+    )
+
+    return defaultShortcuts
+  })
+
+  useEffect(() => {
+
+    localStorage.setItem(
+      "shortcuts",
+      JSON.stringify(shortcuts)
+    )
+
+  }, [shortcuts])
+
+  function editShortcut(shortcut: Shortcut) {
+
+    setTitle(shortcut.name)
+
+    setUrl(shortcut.url)
+
+    setImage(shortcut.image)
+
+    setEditingId(shortcut.id)
+
+    setShowModal(true)
+  }
+
+  function deleteShortcut(id: number) {
+
+    const updatedShortcuts =
+      shortcuts.filter(
+        (shortcut) =>
+          shortcut.id !== id
+      )
+
+    setShortcuts(updatedShortcuts)
+  }
+  
+  function addShortcut() {
+    if (editingId !== null) {
+
+      const updatedShortcuts =
+        shortcuts.map((shortcut) =>
+
+          shortcut.id === editingId
+            ? {
+                ...shortcut,
+
+                name: title,
+
+                url,
+
+                image,
+              }
+            : shortcut
+        )
+
+      setShortcuts(updatedShortcuts)
+      setShowModal(false)
+
+    } else {
+
+      const newShortcut = {
+        id: Date.now(),
+
+        name: title,
+
+        url,
+
+        image:
+          image ||
+          "https://cdn-icons-png.flaticon.com/512/25/25694.png",
+      }
+
+      setShortcuts([
+        ...shortcuts,
+        newShortcut,
+      ])
+
+      setTitle("")
+      setUrl("")
+      setImage("")
+
+      setShowModal(false)
+    }
+  }
   return (
     <div className="mt-8 flex justify-center">
 
@@ -13,9 +132,9 @@ function ShortcutGrid() {
 
         {shortcuts.map((shortcut) => (
 
-          <Card
-            key={shortcut.name}
-            className="relative flex items-center justify-center h-20 w-20 border-none text-white hover:bg-black backdrop-blur-3xl backdrop-grayscale-30 transition cursor-pointer"
+          <Card onClick={() => window.open(shortcut.url)}
+            key={shortcut.id}
+            className="relative flex items-center justify-center h-20 w-20 border-none text-white hover:scale-120 hover:bg-black backdrop-blur-3xl backdrop-grayscale-30 transition cursor-pointer"
           >
 
             <CardContent className="absolute flex flex-col items-center gap-2">
@@ -26,17 +145,65 @@ function ShortcutGrid() {
                 className="h-8 w-8"
               />
 
-              <p className="text-sm">
+              <p className="text-sm flex justify-center items-center">
                 {shortcut.name}
               </p>
 
             </CardContent>
+            <div className="absolute flex items-center justify-center right-1 pb-2 top-0 h-5 w-5"
+            onClick={(e) => e.stopPropagation()}>
+
+              <DropdownMenu>
+
+                <DropdownMenuTrigger>
+
+                  <button className="flex h-8 w-8 items-center justify-center pb-2 text-xl font-bold  text-white">
+                    ...
+                  </button>
+
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent className="bg-black text-white">
+
+                  <DropdownMenuItem className="hover:bg-zinc-700"
+                   onClick={() => editShortcut(shortcut)}>
+                    Edit
+                  </DropdownMenuItem>
+
+                  <DropdownMenuItem className="hover:bg-zinc-700" 
+                  onClick={() => deleteShortcut(shortcut.id)}>
+                    Delete
+                  </DropdownMenuItem>
+
+                </DropdownMenuContent>
+
+              </DropdownMenu>
+
+            </div>
 
           </Card>
 
         ))}
+        <Card onClick={() => setShowModal(true)}
+        className="relative flex items-center justify-center h-20 w-20 border-none text-white hover:bg-black hover:scale-120 backdrop-blur-3xl backdrop-grayscale-30 transition cursor-pointer">
+          <div className="text-5xl  mb-5">+</div> 
+        </Card>
 
       </div>
+
+      <ModalOverlay 
+        title={title}
+        setTitle={setTitle}
+
+        url={url}
+        setUrl={setUrl}
+
+        image={image}
+        setImage={setImage}
+        isOpen={showModal} 
+        onClose={() => setShowModal(false)} 
+        onAdd={addShortcut}
+      />
 
     </div>
   )
--- src/data/Shortcuts.ts
+++ src/data/Shortcuts.ts
@@ -1,30 +1,40 @@
-export const shortcuts = [
+export const defaultShortcuts = [
   {
+    id: 1,
     name: "YouTube",
     image:
       "https://cdn-icons-png.flaticon.com/512/1384/1384060.png",
+    url:"https://www.youtube.com/"
   },
 
   {
+    id: 2,
     name: "Maps",
     image:
       "https://cdn-icons-png.flaticon.com/512/2991/2991231.png",
+    url:"https://www.google.com/maps"
   },
 
   {
+    id: 3,
     name: "Spotify",
     image:
       "https://cdn-icons-png.flaticon.com/512/174/174872.png",
+    url: "https://open.spotify.com/"
   },
 
   {
+    id: 4,
     name: "Reddit",
     image:
       "https://cdn-icons-png.flaticon.com/512/2111/2111589.png",
+    url: "https://www.reddit.com/"
   },
 
   {
+    id: 5,
     name: "Gmail",
-    image:"https://cdn-icons-png.flaticon.com/512/732/732200.png"
+    image:"https://cdn-icons-png.flaticon.com/512/732/732200.png",
+    url:"https://mail.google.com/mail/"
   }
 ]
