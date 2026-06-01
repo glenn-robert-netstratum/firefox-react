3806e255105fface822e9e24bdc6288cb7e2a37e
glenn-robert-netstratum <glennrobert@netstratum.com>
2026-06-01 18:47:11 +0530

    feat(NewsGrid):Added the news api and adjusted the newsgrid layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ShortcutGrid from "./components/ShortcutGrid"
 
 function App() {
   return (
-    <div className="min-h-screen bg-[url('https://images.hdqwalls.com/download/land-rover-defender-octa-2025-pp-1366x768.jpg')] bg-center bg-cover bg-no-repeat">
+    <div className="min-h-screen bg-[url('https://images.hdqwalls.com/download/land-rover-defender-octa-2025-pp-1366x768.jpg')] bg-center bg-cover bg-no-repeat bg-fixed">
       <Header />
       <ShortcutGrid />
       <NewsGrid />
